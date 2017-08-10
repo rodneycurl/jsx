16c4d08e28372bf629392fa529861ea00e00c39a
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-08-10 16:52:07 -0400

    adding new components to app

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    // getinitialState
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish){
+    // update our state
+    const fishes = {...this.state.fishes};
+    // add in our new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] =
+    // this.state.fishes.fish1 = fish
+    // set state
+  }
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Good Food" />
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    )
+  }
+}
+
+export default App;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
-import StorePicker from './components/StorePicker';
+import { BrowserRouter, Match, Miss} from 'react-router';
 
-render(<StorePicker/>, document.querySelector('#main'));
+import './css/style.css';
+import App from './components/App';
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound'
+
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+
+  )
+}
+
+render(<Root/>, document.querySelector('#main'));
