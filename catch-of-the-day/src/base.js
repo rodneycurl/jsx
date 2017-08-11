f20d9c69ba986774ecbf3d0361ece79b89c0d7ae
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-08-11 17:11:43 -0400

    updated catch-of-the-day to locally store information

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCuZdb1Y9FJeJvQ6L8PmeK8zQtVvfHMe20",
+  authDomain: "catch-of-the-day-rodney-curl.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-rodney-curl.firebaseio.com",
+});
+
+export default base;
